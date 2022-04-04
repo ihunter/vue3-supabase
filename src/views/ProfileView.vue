4db07284e3831e45b48f6586155514736fcffa6e
ihunter <ijh.hunter@gmail.com>
2022-04-04 01:07:08 -0400

    profile view WIP

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -40,13 +40,70 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
-    <div>USERNAME: {{ profile.username }}</div>
-    <div>BIRTHDAY: {{ profile.birthday }}</div>
-    <div>BIO: {{ profile.bio }}</div>
-    <div>CREATED_AT: {{ profile.created_at }}</div>
-    <div>UPDATED_AT: {{ profile.updated_at }}</div>
+  <div class="profile">
+    <div class="profile-avatar"></div>
+    <div class="profile-actions">
+      <button>Follow</button>
+    </div>
+    <div class="profile-name">
+      {{ profile.username }}
+    </div>
+    <div class="profile-bio">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea impedit
+      suscipit eligendi aut fugit. Asperiores nesciunt dolores et omnis
+      perspiciatis ea dolor perferendis laborum, magni error odio, ullam,
+      voluptatum nostrum?
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile {
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto auto auto;
+  grid-template-areas:
+    "avatar actions"
+    "name name"
+    "bio bio";
+  .profile-avatar {
+    grid-area: avatar;
+    width: 125px;
+    height: 125px;
+    background-color: aqua;
+    border-radius: 50%;
+    margin-bottom: 1rem;
+  }
+
+  .profile-actions {
+    grid-area: actions;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    button {
+      padding: 0.5rem 1rem;
+      font-weight: bold;
+      background-color: transparent;
+      border: 1px solid var(--primary);
+      color: var(--color-text);
+      border-radius: 9999px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: var(--primary);
+      }
+    }
+  }
+
+  .profile-name {
+    grid-area: name;
+    font-weight: bold;
+  }
+
+  .profile-bio {
+    grid-area: bio;
+  }
+}
+</style>
