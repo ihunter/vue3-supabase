779f789aaa2e1023e5bd76cad93a529f7cf01685
ihunter <ijh.hunter@gmail.com>
2022-05-01 00:53:45 -0400

    profile snippit layout

--- src/components/ProfileSnippit.vue
+++ src/components/ProfileSnippit.vue
@@ -69,42 +69,41 @@ const isUserFollowing = computed(() => {
 <template>
   <div class="profile">
     <div class="profile-avatar"></div>
+
+    <div class="profile-content">
+      <div class="profile-name">
+        {{ profile.username }}
+      </div>
+      <div class="profile-bio">
+        <p v-if="profile.bio">{{ profile.bio }}</p>
+        <p v-else>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+          quae iste a dolorum assumenda culpa ut?
+        </p>
+      </div>
+    </div>
+
     <div class="profile-actions" v-if="!isUsersProfile">
       <button v-if="isUserFollowing" @click="unfollowUser">Unfollow</button>
       <button v-else @click="followUser">Follow</button>
-    </div>
-    <div class="profile-name">
-      {{ profile.username }}
-    </div>
-    <div class="profile-bio">
-      <p v-if="profile.bio">{{ profile.bio }}</p>
-      <p v-else>Bio... if there was one. :(</p>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .profile {
+  display: flex;
   padding: 1rem;
-  display: grid;
   border-bottom: 1px solid var(--border-color);
-  grid-template-columns: auto 1fr;
-  grid-template-rows: auto auto auto;
-  grid-template-areas:
-    "avatar actions"
-    "name name"
-    "bio bio";
+
   .profile-avatar {
-    grid-area: avatar;
-    width: 125px;
-    height: 125px;
+    width: 50px;
+    height: 50px;
     background-color: aqua;
     border-radius: 50%;
-    margin-bottom: 1rem;
   }
 
   .profile-actions {
-    grid-area: actions;
     display: flex;
     justify-content: flex-end;
     align-items: center;
@@ -124,13 +123,15 @@ const isUserFollowing = computed(() => {
     }
   }
 
-  .profile-name {
-    grid-area: name;
-    font-weight: bold;
-  }
+  .profile-content {
+    padding: 0 1rem;
+    flex: 1;
+    .profile-name {
+      font-weight: bold;
+    }
 
-  .profile-bio {
-    grid-area: bio;
+    .profile-bio {
+    }
   }
 }
 </style>
